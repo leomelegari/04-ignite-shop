b5ccbd02d0143b2caac9575c54faf527c153b051
Leonardo <leonardomelegari@gmail.com>
2022-10-30 10:30:58 -0300

    added carrousel using keen-slider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,61 @@
-import { styled } from "../styles";
+import Image from "next/image";
 
-const Button = styled("button", {
-  backgroundColor: "$green500",
-  borderRadius: 4,
-  border: 0,
-  padding: "4px 8px",
-});
+import { useKeenSlider } from "keen-slider/react";
+
+import { HomeContainer, Product } from "../styles/pages/home";
+
+import cam1 from "../assets/camisetas/1.png";
+import cam2 from "../assets/camisetas/2.png";
+import cam3 from "../assets/camisetas/3.png";
+
+import "keen-slider/keen-slider.min.css";
 
 export default function Home() {
-  return <Button>Enviar</Button>;
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+  return (
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={cam1} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={cam2} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={cam3} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={cam3} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  );
 }
 
 // file system routing
